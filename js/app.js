59ff73d6810dab0a0ead6e5fc60507bfe3c5d6cc
Nobu <rockmusic2090@gmail.com>
2019-09-04 20:18:50 -0700

    first commit and add HTML, CSS, JS

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+function addButtonValue(whichButton){
+  document.getElementById('result').value += whichButton;
+  console.log("it's working")
+}
+
+
+function clearScreen(){
+  document.getElementById('result').value ="";
+}
+
+function equalsAnswer(){
+   document.getElementById('result').value = eval(document.getElementById('result').value);
+}
+
+function change(){
+  
+  if( document.getElementById('result').value>0){
+    document.getElementById('result').value = "-"+ document.getElementById('result').value;
+  }else if(document.getElementById('result').value<0){
+    document.getElementById('result').value = document.getElementById('result').value*-1;
+  }
+}
+
+// I'm still working on it
+function parcentage(){
+  document.getElementById('result').value = document.getElementById('result').value;
+}
+
+
+
+
+
+
